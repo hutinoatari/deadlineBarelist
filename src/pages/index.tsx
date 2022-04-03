ea5ab6542daf1e59faf45766a52c3fd2b10278d1
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-04-04 04:22:36 +0900

    ガワと適当な中身を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 const TopPage: FC<{}> = () => {
-    return <h1>期限ギリギリスト</h1>;
+    const [taskDatas, setTaskDatas] = useState<string[]>([]);
+    return (
+        <>
+            <header>
+                <h1>期限ギリギリスト</h1>
+            </header>
+            <main>
+                <button
+                    onClick={() =>
+                        setTaskDatas([
+                            ...taskDatas,
+                            "" + Math.floor(Math.random() * 100),
+                        ])
+                    }
+                >
+                    追加
+                </button>
+                <table>
+                    {taskDatas.map((e, i) => (
+                        <tr key={i}>
+                            <td>{e}</td>
+                        </tr>
+                    ))}
+                </table>
+            </main>
+            <footer>
+                <p>
+                    <small>&copy;2022 淵野アタリ</small>
+                </p>
+            </footer>
+        </>
+    );
 };
 
 export default TopPage;
